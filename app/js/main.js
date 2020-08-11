318ec9c1db578ba4c14e127f7235a2105aec4e3c
Anvar Turakulov <anvarsam@mail.ru>
2020-08-12 03:55:05 +0500

    added editor page and add other changs

--- app/js/main.js
+++ app/js/main.js
@@ -29,4 +29,52 @@ $(function(){
         $('.ico-show-btn').toggleClass('rotate__180');
     }); 
 
+    $('.arrow__work').click(function(event){
+        if ($('.rightside__work-inner').hasClass('open')){
+            $('.rightside__work-inner').removeClass('open');
+            $('.rightside__work-inner').slideDown(300);
+            $('.arrow__work').toggleClass('arrow-down-right');
+        }else {
+            $('.rightside__work-inner').addClass('open');
+            $('.rightside__work-inner').slideUp(300);
+            $('.arrow__work').toggleClass('arrow-down-right');
+        };
+      });
+
+    $('.arrow__avtorepair').click(function(event){
+        if ($('.rightside__avtorepair-inner').hasClass('open')){
+            $('.rightside__avtorepair-inner').removeClass('open');
+            $('.rightside__avtorepair-inner').slideDown(300);
+            $('.arrow__avtorepair').toggleClass('arrow-down-right');
+        }else {
+            $('.rightside__avtorepair-inner').addClass('open');
+            $('.rightside__avtorepair-inner').slideUp(300);
+            $('.arrow__avtorepair').toggleClass('arrow-down-right');
+        };
+      });
+
+      $('.arrow__account').click(function(event){
+        if ($('.rightside__account-inner').hasClass('open')){
+            $('.rightside__account-inner').removeClass('open');
+            $('.rightside__account-inner').slideDown(300);
+            $('.arrow__account').toggleClass('arrow-down-right');
+        }else {
+            $('.rightside__account-inner').addClass('open');
+            $('.rightside__account-inner').slideUp(300);
+            $('.arrow__account').toggleClass('arrow-down-right');
+        };
+      });
+
+      $('.arrow__answer').click(function(event){
+        if ($('.rightside__answer-inner').hasClass('open')){
+            $('.rightside__answer-inner').removeClass('open');
+            $('.rightside__answer-inner').slideDown(300);
+            $('.arrow__answer').toggleClass('arrow-down-right');
+        }else {
+            $('.rightside__answer-inner').addClass('open');
+            $('.rightside__answer-inner').slideUp(300);
+            $('.arrow__answer').toggleClass('arrow-down-right');
+        };
+      });
+
 });
