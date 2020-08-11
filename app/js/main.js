5dbd58c14d9b772fcaec2a307c8ed5fdaf08dd73
Anvar Turakulov <anvarsam@mail.ru>
2020-08-12 01:10:45 +0500

    added middle side

--- app/js/main.js
+++ app/js/main.js
@@ -7,4 +7,21 @@ $(function(){
             $('body').addClass('dark__theme');
         }
       });
+    
+    $('.leftside__person-name').click(function(event){
+        if ($('.leftside__person__inner').hasClass('open')){
+            $('.leftside__person__inner').removeClass('open');
+            $('.leftside__person__inner').slideDown(300);
+            $('.arrow__name').toggleClass('arrow-down');
+        }else {
+            $('.leftside__person__inner').addClass('open');
+            $('.leftside__person__inner').slideUp(300);
+            $('.arrow__name').toggleClass('arrow-down');
+        };
+      });
+       
+      $('.action__type').on('click',function(){
+        $('.action__list').slideToggle();
+        }); 
+      
 });
